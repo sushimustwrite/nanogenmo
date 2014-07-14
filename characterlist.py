0e79f0cbdbdc73a5713ec6604f6bc9ab73133f86
Sujin/Sushi <sushimustwrite@gmail.com>
2014-07-13 23:05:25 -0400

    character creation
    need catchphrases/descriptions

---
+++ characterlist.py
@@ -0,0 +1,120 @@
+Character("Protagovitch", printutils.male, "(That's #obj!)", location=None, "not very tall and not very handsome, but with a heart of gold", items=None)
+
+had a past lover (Protagzekslova) in his village, but she died tragically when she licked a poisonous mushroom
+
+dad died when he was eaten by a grue. Protagovitch was 14 at the time.
+
+killed the grue as payback
+
+he has a heart of gold because he's been hurt so many times. He's very trusting.
+
+loving and emotionally strong
+
+educated at home by his mother
+
+knows how to read and write
+
+has a perfect memory
+
+no siblings
+
+fell out of animate tree as a child and broke several bones, and acquired a cool-looking scar on his face
+
+still in touch with several childhood friends as they still live in the village
+
+once ate a dozen crickets at once to win a bet
+
+
+Character("Antagovitch", printutils.male, "catchphrase", location=None, "tall and attractive with a hunger for power", items=None)
+
+never saw his parents much as a child; they usually left him with nannies while they spent away their inheritance
+
+dreamed of taking over the world
+
+after finishing his education, he heard of the empire far away and walked the entire trip there
+
+beat the demon in an arm-wrestling match on his way to the empire, received charisma in exchange for his hair
+
+somehow looks even better bald than with hair
+
+showed up at the castle ragged and with a broken nose after a fight with a troll (his nose is still a little crooked)
+
+he offered to be their servant, then worked his way up as people died adn he sucked up to people
+
+wanted to take over the world so everyone could be his friend
+
+his childhood friends all died mysterious deaths. he may or may not have been involved.
+
+never had a girlfriend or boyfriend
+
+
+Character("Protagslova", printutils.female, "A vos souhaits", location=None, "wavy red hair, piercing blue eyes, and a smile that melts hearts", items=None)
+
+loves trees and treehouses
+
+when she was five, her brother cut all her hair off. it grew back instantly.
+
+witnessed Protagovitch kill the grue but could barely see it was him
+
+super night vision
+
+
+Character("Dolga", printutils.neuter, "That's an awfully long name. That's an awfully long dragon.", location=None, "shiny red and breathe", items="scale")
+
+Character("Demon", printutils.neuter, "", location=None, "carries a bright red pitchfork and is unusually cute for a demon", items=None)
+
+Character("Rodent of Unusual Size", printutils.it, "", location=None, "", items=None)
+
+Character("Mom", printutils.female, "Would you like a cuppa?", location=None, "", items=None)
+
+Character("Donovitch", printutils.male, "It's dangerous to go alone! Take this.", location=None, "", items=None)
+
+Character("Snezana", printutils.female, "", location=None, "jet black hair, big blue eyes", items=None)
+
+Character("Baba Yaga", printutils.female, "", location=None, "", items=None)
+
+Character("Hammerhead Shark", printutils.it, "You look like a nail.", location=None, "", items=None)
+
+Character("Crab King", printutils.male, "Crabhammer!!!", location=None, "the largest of the crabs", items=None)
+
+Character("Firebird", printutils.female, "born of ash", location=None, "red fiery wings and a long beak that can peck through anything", items=None)
+
+Character("Shtativ the Three-Legged Ghost", printutils.male, "Are you scared yet?", location=None, "perfectly ordinary except for a third leg and that whole ghost thing", items=None)
+
+Character("Pesoky the Sandworm", printutils.it, "", location=None, "", items=None)
+
+Character("Vila the Vila", printutils.female, "", location=None, "", items=None)
+
+Character("Pauk the Giant Spider", printutils.it, "The stench and the dread.", location=None, "big hairy spider with eight long legs", items=None)
+
+Character("Psoglav", printutils.male, "", location=None, "", items=None)
+
+Character("Hag of Odinokorod", printutils.female, "", location=None, "", items=None)
+
+Character("King Odinovski the First", printutils.male, "", location=None, "", items=None)
+
+Character("King Zvaski the Second", printutils.male, "Hope you like it hot.", location=None, "", items=None)
+
+Character("Queen Triski the Third", printutils.female, "Ditto.", location=None, "", items=None)
+
+Character("King Chetyrski the Fourth", printutils.male, "", location=None, "", items=None)
+
+Character("Quing Pyatski the Fifth", printutils.neuter, "", location=None, "", items=None)
+
+Character("Queen Shestski the Sixth", printutils.female, "Up on Cloud Nine", location=None, "", items=None)
+
+Character("Centaurskaya", printutils.female, "Mars is bright tonight.", location=None, "", items=None)
+
+Character("Karzelek the Dwarf", printutils.male, "Where's Snezana?", location=None, "", items=None)
+
+Character("Ziz", printutils.neuter, "", location=None, "", items=None)
+
+Character("Troll", printutils.male, "", location=None, "", items=None)
+
+Character("Goat King", printutils.male, "", location=None, "", items=None)
+
+Character("Werewolf", printutils.male, "", location=None, "", items=None)
+
+Character("Vucagorod", printutils.male, "", location=None, "", items=None)
+
+Character("Gamayun the Bird", printutils.female, "Wings and breasts, just like chicken", location=None, "half-bird, half-woman with all the best features of both", items=None)
