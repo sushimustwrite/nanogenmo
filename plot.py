d74d5577f9d408451ed58c805fbb3252e6137492
David Rutter <quintopia@gmail.com>
2014-07-20 23:46:00 -0400

    I don't remember

--- plot.py
+++ plot.py
@@ -1,6 +1,10 @@
-states = [];
-major_characters = [];
-items = [];
-locations = [];
-
+# -*- coding: utf-8 -*-
+states = []
+major_characters = []
+items = []
+locations = []
+p_rand_encounters = []
+a_rand_encounters = []
+protagonist = None
+antagonist = None
 #add stuff to the plot here
