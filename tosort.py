2ba2cabc64b7dd91dee5ae305527f943b0f0ccfb
sushimustwrite <sushimustwrite@gmail.com>
2014-07-27 15:42:51 -0400

    added fn for dolgo to become dooolgo or dooooooolgo. will add to appropriate file later

---
+++ tosort.py
@@ -0,0 +1,12 @@
+# dolga's randomized o's
+# replace dolga with appropriate character dict
+
+import random, string
+
+def dolgo_replace(search_string):
+    search_query = "Dolgo "
+    for search_query in search_string:
+        search_string = search_string.replace("Dolgo ", "D"+"o"*random.randint(3,20)+"lgo ", 1)
+    print search_string
+
+dolgo_replace("Dolgo your mom Dolgo protagodolgo rawr Dolgo Dolgo Dolgo alalala") 
