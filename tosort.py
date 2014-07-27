b0bb4ed5f616faab46caf73bba3bb460bc7a69e0
David Rutter <quintopia@gmail.com>
2014-07-27 15:51:53 -0400

    blah

--- tosort.py
+++ tosort.py
@@ -7,7 +7,7 @@ import random, string
 def dolgo_replace(search_string):
     search_query = "Dolgo"
     for search_query in search_string:
-        search_string = search_string.replace("Dolgo", "D"+"o"*random.randint(3,20)+"lgo ", 1)
+        search_string = search_string.replace("Dolgo", "D"+"o"*random.randint(3,20)+"lgo", 1)
     print search_string
 
 dolgo_replace("Dolgo your mom Dolgo protagodolgo rawr Dolgo Dolgo Dolgo alalala") 
