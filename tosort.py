8a1a2304a09cd220692d0d2e9a4608e1c20b6eac
David Rutter <quintopia@gmail.com>
2014-07-27 15:50:27 -0400

    nm

--- tosort.py
+++ tosort.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 # dolga's randomized o's
 # replace dolga with appropriate character dict
 
 import random, string
 
 def dolgo_replace(search_string):
-    search_query = "Dolgo "
+    search_query = "Dolgo"
     for search_query in search_string:
         search_string = search_string.replace("Dolgo ", "D"+"o"*random.randint(3,20)+"lgo ", 1)
     print search_string
